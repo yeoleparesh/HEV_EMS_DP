a71989036e8df50cad72382e4e9fa8d18e5c3ec8
Paresh Yeole <yeoleparesh@gmail.com>
2020-06-14 21:56:18 +0200

    Add files via upload

--- RUN_HEV.m
+++ RUN_HEV.m
@@ -1,7 +1,11 @@
-function [P_batt_act, P_eng_act, FC_act, SOC_act] = RUN_HEV(SOC_init)
+function [P_batt_act, P_eng_act, FC_act, SOC_act]=RUN_HEV(SOC_init,N,SOC_grid,u_opt,P_dem)
     SOC_act(1) = SOC_init;
+    ts =1;
+    U_oc = 320;
+    fl_wt_en = 0.001;
+    Q_batt = 18000;
     for i = 1:N-1
-        P_batt_act(i) = interp1(SOC_grid,u_opt(:,i),SOC_sim(i));
+        P_batt_act(i) = interp1(SOC_grid,u_opt(:,i),SOC_act(i));
         P_eng_act(i) = P_dem(i) - P_batt_act(i);
         FC_act(i) = (ts*fl_wt_en*P_eng_act(i))/(eng_eff(P_eng_act(i))); 
         SOC_act(i+1) = SOC_act(i) - ((ts*P_batt_act(i))/(Q_batt*U_oc));       
--- eng_eff.m
+++ eng_eff.m
@@ -1,10 +1,10 @@
 function eff = eng_eff(P_eng)
-    a = 5*e-08;
-    b = -6*e-06;
-    c = 2.6*e-04;
-    d = -5.8*e-03;
-    e = 6.8*e-02;
-    f = 2.6*e-05;
-    x = P_eng*e-03;                   % W to KW
+    a =  5e-08;
+    b = -6e-06;
+    c = 2.6e-04;
+    d = -5.8e-03;
+    e = 6.8e-02;
+    f = 2.6e-05;
+    x = P_eng/1000;                   % W to KW
     eff = a*x.^5 + b*x.^4 + c*x.^3 + d*x.^2 + e*x + f;
 end
